3b08910f0a963c3a86beaedf256d2b8c9e0f8254
AbdullahJUTT <abdullahahmad319.319@gmail.com>
2023-08-09 16:03:09 +0500

    Add existing project files to Git

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> _responses = new ();
+        public static IEnumerable<GuestResponse> list => _responses;
+        public static void AddRespose(GuestResponse response)
+        {
+            Console.WriteLine(response);
+            _responses.Add(response);
+        }
+    }
+}
